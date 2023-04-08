08dc04ab0ea07bbb8ac8f40fc93587eaf41a7807
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-04-08 16:35:11 -0400

    Create password field matching logic
    Javascript code is created, which will give the password fields a class
    based on whether or not the two fields have the same thing entered in
    both of them. The classes determine the border colors they get.

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-let inputs = document.querySelectorAll("input");
+const inputs = document.querySelectorAll("input");
 inputs.forEach((input) => {
-  input.addEventListener("input", () => {
-    input.classList.add("clicked");
-  });
+  input.addEventListener(
+    "input",
+    () => {
+      input.classList.add("entered");
+    },
+    { once: true }
+  );
+});
+
+const pass1 = document.querySelector("#pass");
+const pass2 = document.querySelector("#pass_confirm");
+
+pass2.addEventListener("input", () => {
+  if (pass1.value === pass2.value) {
+    console.log(pass1.value, pass2.value);
+    pass1.classList.remove("no");
+    pass2.classList.remove("no");
+
+    pass1.classList.add("yes");
+    pass2.classList.add("yes");
+  } else {
+    pass1.classList.remove("yes");
+    pass2.classList.remove("yes");
+
+    pass1.classList.add("no");
+    pass2.classList.add("no");
+  }
 });
