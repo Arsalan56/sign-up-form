ff721f6d41047b30f09565e4b7733febcd36e2cc
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-04-09 18:23:51 -0400

    Make button not submit when passwords don't match
    The submit button is changed to a normal button when the javascript
    code detects that the two password fields don't match up. This makes it
    so the user can not make an account with the two fields not being the
    same.

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ inputs.forEach((input) => {
 const pass1 = document.querySelector("#pass");
 const pass2 = document.querySelector("#pass_confirm");
 
+const submit = document.querySelector("button");
+
 pass2.addEventListener("input", () => {
   if (pass1.value === pass2.value) {
     console.log(pass1.value, pass2.value);
@@ -20,11 +22,17 @@ pass2.addEventListener("input", () => {
 
     pass1.classList.add("yes");
     pass2.classList.add("yes");
+
+    // Make button work when passwords match
+    submit.setAttribute("type", "submit");
   } else {
     pass1.classList.remove("yes");
     pass2.classList.remove("yes");
 
     pass1.classList.add("no");
     pass2.classList.add("no");
+
+    // Make button not work when passwords don't match
+    submit.setAttribute("type", "button");
   }
 });
