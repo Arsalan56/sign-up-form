62f7b8def96ea4a90bad6b36f80bd0f9f2266db5
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-04-15 16:51:28 -0400

    Make warning under re-enter password field work
    All the red warning texts appear appropriately whenever they are
    needed. The main parts of the program are completed as of now. The html
    is changed in order to make the user redirect to the same page instead
    of an error page when the form successfully submits.

--- script.js
+++ script.js
@@ -12,19 +12,20 @@ inputs.forEach((input) => {
 
 const pass = document.querySelectorAll("[type='password']");
 
-pass1 = pass[0];
-pass2 = pass[1];
+let pass1 = pass[0];
+let pass2 = pass[1];
 
 const submit = document.querySelector("button");
+const unmatched = document.querySelector(".pass-2-confirm");
 
 for (i = 0; i < pass.length; i++) {
   pass[i].addEventListener("input", () => {
     if (!pass1.validity.valid || !pass2.validity.valid) {
       pass1.classList.remove("yes");
       pass1.classList.add("no");
-
       pass2.classList.remove("yes");
       pass2.classList.add("no");
+      submit.setAttribute("type", "button");
     }
     if (
       pass1.value.length > 0 &&
@@ -35,12 +36,33 @@ for (i = 0; i < pass.length; i++) {
     ) {
       pass1.classList.remove("no");
       pass1.classList.add("yes");
-
+      unmatched.style.visibility = "hidden";
       pass2.classList.remove("no");
       pass2.classList.add("yes");
 
       // Make button work when passwords match
       submit.setAttribute("type", "submit");
+    } else {
+      unmatched.style.visibility = "visible";
+
+      pass1.classList.add("no");
+      pass1.classList.remove("yes");
+      pass2.classList.add("no");
+      pass2.classList.remove("yes");
+      submit.setAttribute("type", "button");
+    }
+  });
+}
+
+const red = document.getElementsByClassName("red");
+const texts = document.querySelectorAll("form > div > div > div > p");
+
+for (let i = 0; i < red.length; i++) {
+  red[i].addEventListener("input", () => {
+    if (!red[i].validity.valid && red[i].value.length > 0) {
+      texts[i].style.visibility = "visible";
+    } else {
+      texts[i].style.visibility = "hidden";
     }
   });
 }
